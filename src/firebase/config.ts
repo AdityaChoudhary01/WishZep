5388ebeda60d4f78a8741883d05de3a42b8f56dc
Aadi Choudhary <Adityanain552@gmail.com>
2026-02-04 12:57:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-920990075-d21bb",
+  "appId": "1:130121987654:web:ae2484635687bc1891fc47",
+  "apiKey": "AIzaSyAoTdblgeon7YgBW3dypdaeunrFVyhQoi4",
+  "authDomain": "studio-920990075-d21bb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "130121987654"
+};
